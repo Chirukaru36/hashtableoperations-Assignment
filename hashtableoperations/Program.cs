f150bee80bb4eba8cb0fb2a4b2498358e83b1556
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:07:54 +0530

    Add project files.

---
+++ hashtableoperations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashtableoperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashTable1 = new Hashtable();
+
+           
+            hashTable1.Add("Key1", "Value1");
+            hashTable1.Add("Key2", "Value2");
+            hashTable1.Add("Key3", "Value3");
+
+            
+            Console.WriteLine("First Hash Table:");
+            foreach (DictionaryEntry entry  in hashTable1)
+            {
+                Console.WriteLine(entry.Key + " " + entry.Value);
+            }
+            
+            
+            Hashtable hashTable2 = new Hashtable(hashTable1);
+
+
+
+
+            
+            Console.WriteLine("\nSecond Hash Table (copied from the first):");
+             
+
+            
+            hashTable1.Remove("Key2");
+
+            foreach(DictionaryEntry entry in hashTable2)
+            {
+                Console.WriteLine(entry.Key + " " + entry.Value );
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+ 
